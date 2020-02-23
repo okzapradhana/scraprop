2545c69918bb77d2079a5f615f55964f33081cae
okzapradhana <okzamahendra29@gmail.com>
2020-02-24 05:05:29 +0700

    using celery to parallel scrapping

--- Goddard/jendela.py
+++ Goddard/jendela.py
@@ -56,7 +56,7 @@ def scrap_link():
     return list_urls
 
 @app.task
-def scrap_each_page(list_urls:[]):
+def scrap_each_page(url):
     load_dotenv()
     executable_path = os.getenv('EXECUTABLE_PATH')
     driver = webdriver.Chrome(executable_path)
@@ -73,88 +73,87 @@ def scrap_each_page(list_urls:[]):
     list_conditions, list_estimation_prices = ([] for i in range(12))
 
     #visit each page
-    for each_url in list_urls:
-        print('Current url: ', each_url, '\n')
-        driver.get(each_url)
-        try:
-            present_element = EC.presence_of_element_located((By.CLASS_NAME, 'container'))
-            WebDriverWait(driver, timeout_wait).until(present_element)
-        except TimeoutException:
-            print('Time out waiting page to Load')
-        finally:
-            curr_time = datetime.now(timezone('Asia/Jakarta'))
-            time_taken = curr_time.strftime(format_time)
+    print('Current url: ', url, '\n')
+    driver.get(url)
+    try:
+        present_element = EC.presence_of_element_located((By.CLASS_NAME, 'container'))
+        WebDriverWait(driver, timeout_wait).until(present_element)
+    except TimeoutException:
+        print('Time out waiting page to Load')
+    finally:
+        curr_time = datetime.now(timezone('Asia/Jakarta'))
+        time_taken = curr_time.strftime(format_time)
 
-            list_time_taken.append(time_taken)
+        list_time_taken.append(time_taken)
 
-            unit_detail = driver.find_element(By.CSS_SELECTOR, "div[class='container']")
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-            
-            driver.implicitly_wait(5)
-            modal = driver.find_elements(By.CLASS_NAME, 'modal__panel')
-            if(len(modal) > 0):
-                btn = driver.find_element(By.CLASS_NAME, 'helper__close__btn')
-                btn.click()
-            else:
-                print('Modal not displayed')
+        unit_detail = driver.find_element(By.CSS_SELECTOR, "div[class='container']")
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        
+        driver.implicitly_wait(5)
+        modal = driver.find_elements(By.CLASS_NAME, 'modal__panel')
+        if(len(modal) > 0):
+            btn = driver.find_element(By.CLASS_NAME, 'helper__close__btn')
+            btn.click()
+        else:
+            print('Modal not displayed')
 
-            unit_name = unit_detail.find_element(By.CSS_SELECTOR, "div[id='units'] > h1").text
-            bedroom = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(1)").text.strip()
-            bathroom = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(2)").text.strip()
-            condition = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(3)").text.strip()
-            area = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(4)").text.strip()
-            floor = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(5)").text.strip()
-            tower = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(6)").text.strip()
-            rent_periods = unit_detail.find_elements(By.CSS_SELECTOR, ".price-card ul[class='price-btn-tabs'] >li")
-            rent_keys = ["full", "monthly"]
-            
-            dict_rent_period = {}
+        unit_name = unit_detail.find_element(By.CSS_SELECTOR, "div[id='units'] > h1").text
+        bedroom = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(1)").text.strip()
+        bathroom = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(2)").text.strip()
+        condition = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(3)").text.strip()
+        area = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(4)").text.strip()
+        floor = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(5)").text.strip()
+        tower = unit_detail.find_element(By.CSS_SELECTOR, ".gridded--list li:nth-of-type(6)").text.strip()
+        rent_periods = unit_detail.find_elements(By.CSS_SELECTOR, ".price-card ul[class='price-btn-tabs'] >li")
+        rent_keys = ["full", "monthly"]
+        
+        dict_rent_period = {}
 
-            for rent_period in rent_periods:
-                anchor_href = rent_period.find_element(By.CSS_SELECTOR, ".price-tab")
-                rent_month_text = rent_period.find_element(By.CSS_SELECTOR, ".price-tab > span").text
-                anchor_href.click()
+        for rent_period in rent_periods:
+            anchor_href = rent_period.find_element(By.CSS_SELECTOR, ".price-tab")
+            rent_month_text = rent_period.find_element(By.CSS_SELECTOR, ".price-tab > span").text
+            anchor_href.click()
 
-                rent_price = unit_detail.find_elements(By.CSS_SELECTOR, ".price-content .star-price")
-                year_month_price = [ price.text.strip() for price in rent_price if price.text != '' ]
-                split_slash_price = [ price.split('/')[0] for price in year_month_price ] #get price only
-                dict_rent_price = dict((key, price.strip()) for price, key in zip(split_slash_price, rent_keys))
-                dict_rent_months = {rent_month_text:dict_rent_price}
-                dict_rent_period.update(dict_rent_months)
+            rent_price = unit_detail.find_elements(By.CSS_SELECTOR, ".price-content .star-price")
+            year_month_price = [ price.text.strip() for price in rent_price if price.text != '' ]
+            split_slash_price = [ price.split('/')[0] for price in year_month_price ] #get price only
+            dict_rent_price = dict((key, price.strip()) for price, key in zip(split_slash_price, rent_keys))
+            dict_rent_months = {rent_month_text:dict_rent_price}
+            dict_rent_period.update(dict_rent_months)
 
-            list_unit_names.append(unit_name)
-            list_bedrooms.append(bedroom)
-            list_bathrooms.append(bathroom)
-            list_areas.append(area)
-            list_floors.append(floor)
-            list_towers.append(tower)
-            list_rent_prices.append(dict_rent_period)
-            list_conditions.append(condition)
+        list_unit_names.append(unit_name)
+        list_bedrooms.append(bedroom)
+        list_bathrooms.append(bathroom)
+        list_areas.append(area)
+        list_floors.append(floor)
+        list_towers.append(tower)
+        list_rent_prices.append(dict_rent_period)
+        list_conditions.append(condition)
 
-            unit_facilities = unit_detail.find_elements(By.CSS_SELECTOR, "div[id='facility'] .facility-text")
-            list_facil_unit = [each_facil.text for each_facil in unit_facilities]
+        unit_facilities = unit_detail.find_elements(By.CSS_SELECTOR, "div[id='facility'] .facility-text")
+        list_facil_unit = [each_facil.text for each_facil in unit_facilities]
 
-            apart_facilities = unit_detail.find_elements(By.CSS_SELECTOR, "div[id='apartmentfacilities'] .facility-text")
-            list_facil_apart = [each_facil.text for each_facil in apart_facilities]
+        apart_facilities = unit_detail.find_elements(By.CSS_SELECTOR, "div[id='apartmentfacilities'] .facility-text")
+        list_facil_apart = [each_facil.text for each_facil in apart_facilities]
 
-            list_facilities_unit.append(list_facil_unit)
-            list_facilities_apart.append(list_facil_apart)
+        list_facilities_unit.append(list_facil_unit)
+        list_facilities_apart.append(list_facil_apart)
 
-            monthly_price = unit_detail.find_elements(By.CSS_SELECTOR, '#monthlyfees > p')
-            split_space_price = [ charge_text.text for charge_text in monthly_price  ]
-            split_colon_price = [ tuple(each_price.split(':')) for each_price in split_space_price ]
-            dict_price = dict((estimasi.strip(), price.strip()) for estimasi, price  in split_colon_price)
+        monthly_price = unit_detail.find_elements(By.CSS_SELECTOR, '#monthlyfees > p')
+        split_space_price = [ charge_text.text for charge_text in monthly_price  ]
+        split_colon_price = [ tuple(each_price.split(':')) for each_price in split_space_price ]
+        dict_price = dict((estimasi.strip(), price.strip()) for estimasi, price  in split_colon_price)
 
-            list_estimation_prices.append(dict_price)
+        list_estimation_prices.append(dict_price)
 
 
         df_home = pd.DataFrame({'datestamp': list_time_taken, 'nama_unit': list_unit_names,
-                            'kamar_tidur': list_bedrooms, 'kamar_mandi': list_bathrooms,
-                            'harga': list_rent_prices,
-                            'luas_bangunan': list_areas, 'tower': list_towers, 'lantai': list_floors,
-                            'condition': list_conditions, 'fasilitas_unit': list_facilities_unit,
-                            'fasilitas_apartemen': list_facilities_apart,
-                            'estimasi_harga': list_estimation_prices})
+                        'kamar_tidur': list_bedrooms, 'kamar_mandi': list_bathrooms,
+                        'harga': list_rent_prices,
+                        'luas_bangunan': list_areas, 'tower': list_towers, 'lantai': list_floors,
+                        'condition': list_conditions, 'fasilitas_unit': list_facilities_unit,
+                        'fasilitas_apartemen': list_facilities_apart,
+                        'estimasi_harga': list_estimation_prices})
 
         df_home.to_csv('../files/jendela_'+str(now_timestamp)+'_.csv')
 
--- Goddard/main.py
+++ Goddard/main.py
@@ -1,15 +1,15 @@
 import click
 import travelio
 from jendela import scrap_link, scrap_each_page
+from celery import group
 
 @click.command()
 @click.option('--web', default='jendela', help='website name to scrap')
 def scrap(web):
     if web == 'jendela':
         list_urls = scrap_link()
-        rmq = scrap_each_page.delay(list_urls)
-        rmq.ready()
-        rmq.get()
+        rmq = group(scrap_each_page.s(url) for url in list_urls)
+        result = rmq.apply_async()
     elif web == 'travelio':
         travelio.scrap()
 
